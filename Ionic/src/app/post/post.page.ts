f73c11ded45555dbf0b710d2af66c074e27d84ab
João Pedro Silva <joaopedro@MacBook-Air-de-Joao.local>
2020-01-30 21:11:12 -0300

    #detalhes do post

--- Ionic/src/app/post/post.page.ts
+++ Ionic/src/app/post/post.page.ts
@@ -6,29 +6,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.page.scss'],
 })
 export class PostPage implements OnInit {
-	
+
 	constructor() { }
- 	dadosPost:object = {
-  	name: 'João',
-  	title:'Otimo episodio',
-  	text: 'sdlkfjasiocnaosicdbauiscdbiasudhasuio',
-  	likes: 0,
-  	dislikes: 0,
-  	reacted: false, 
-  	episode:{
-  		serie: 'Sei la',
-  		number: 1, 
-  	},
-  	comment:{
-  		userName: 'Lol',
-  		text: 'sdfhsdocvnjioasdcnklfhio',
-  	}
+  dadosPost:object = [{
+  name: 'João',
+  title:'Otimo episodio',
+  text: 'sdlkfjasiocnaosicdbauiscdbiasudhasuio',
+  likes: 0,
+  dislikes: 0,
+  disliked: false,
+  liked: false,
+  spoiler: true,
+  episode:{
+    serie: 'Sei la',
+    number: 1,
+  },
+  comment:{
+    userName: 'Lol',
+    text: 'sdfhsdocvnjioasdcnklfhio',
   }
-  	like(dadosPost){
-  		dadosPost.reacted = !(dadosPost.reacted);
-  		dadosPost.likes++;
-  	}
+  },
+  {
+  name: 'João',
+  title:'Otimo episodio',
+  text: 'sdlkfjasiocnaosicdbauiscdbiasudhasuio',
+  likes: 0,
+  dislikes: 0,
+  disliked: false,
+  liked: false,
+  spoiler: false,
+  episode:{
+  serie: 'Sei la',
+  number: 1,
+  },
+  comment:{
+  userName: 'Lol',
+  text: 'sdfhsdocvnjioasdcnklfhio',
+  }
+  }
+
+  ];
+  like(dadosPost){
+    if (dadosPost.liked) {
+      dadosPost.likes--;
+    }
+    if(((!(dadosPost.liked)) && dadosPost.disliked) || ((!(dadosPost.liked)) && (!(dadosPost.disliked)))){
+      dadosPost.likes++;
+      if(dadosPost.dislikes!=0)
+        dadosPost.dislikes--;
+
+      dadosPost.disliked = false;
+    }
+    dadosPost.liked = !(dadosPost.liked);
+    if(!(dadosPost.liked) && !(dadosPost.disliked)){
+
+    }
+  }
+  dislike(dadosPost){
+    if (dadosPost.disliked) {
+      dadosPost.dislikes--;
+
+    }
+    if((!(dadosPost.disliked)) && dadosPost.liked || ((!(dadosPost.disliked)) && (!(dadosPost.liked)) )){
+      dadosPost.dislikes++;
+      if(dadosPost.likes!=0)
+        dadosPost.likes--;
+      dadosPost.liked = false;
+    }
+    dadosPost.disliked = !(dadosPost.disliked);
+  }
   	ngOnInit() {
   }
 }
-
